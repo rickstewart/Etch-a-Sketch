e48f0f35565f657a74ece6cd7942ec52b0016d85
rickstewart <rick.stewart@ns.sympatico.ca>
2014-12-24 10:19:02 -0400

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 /**
  * Created by Rick on 18/12/2014.
  */
+
+var $boxWidth = 500;
+var $numSquaresAcross = 80;
+function makeSquares() {
+    var $numberSquares = $numSquaresAcross * $numSquaresAcross;                 // find total number of squares.
+    var $squareSize = ($boxWidth - $numSquaresAcross * 2) / $numSquaresAcross;  // subtract 2 px per square for border of 1 px per side.
+    for (var i = 0; i < $numberSquares; i++) {
+        $('#squaresWrapper').append('<div class="newSquare"></div>');
+    }
+    $('.newSquare').height($squareSize);
+    $('.newSquare').width($squareSize);
+};
+
+makeSquares();
+
+$('.newSquare').mouseleave(function(){
+    $(this).css('background-color', 'red');
+});
